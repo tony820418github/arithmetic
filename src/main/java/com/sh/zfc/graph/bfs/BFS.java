e6373692557d3543b194c2d15030d52875e527cb
zhangfanchao <32469203@qq.com>
2018-11-23 16:15:02 +0800

    create project

---
+++ src/main/java/com/sh/zfc/graph/bfs/BFS.java
@@ -0,0 +1,31 @@
+package com.sh.zfc.graph.bfs;
+/*
+*
+* */
+import javax.xml.soap.Node;
+
+public class BFS {
+    /**
+     * 生成广度优先算法数
+     */
+    public Node createBFSTree(){
+        return null;
+    }
+
+    /**
+    * 计算两点之间的一条最短路径,邻接矩阵法表示图
+    * */
+    public String shortestPath(Point start , Point end,int[][] map){
+        return null;
+    }
+
+    /*
+
+     */
+    public static  class Point{
+        public int x,y;
+
+    }
+
+
+}
